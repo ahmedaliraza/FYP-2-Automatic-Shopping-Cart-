c4135aa7e29963397ae5d9c1398936742c1f3d7b
Bilal Ahmed <49191880+bilal-ahmed321@users.noreply.github.com>
2019-10-27 16:38:00 +0500

    Update session.php

--- LOGIN(CARTIGO)form/session.php
+++ LOGIN(CARTIGO)form/session.php
@@ -1,10 +1,8 @@
 <?php
-// mysqli_connect() function opens a new connection to the MySQL server.
 $conn = mysqli_connect("localhost", "root", "", "cartigo");
-session_start();// Starting Session
-// Storing Session
+session_start();
 $user_check = $_SESSION['login_user'];
-// SQL Query To Fetch Complete Information Of User
+
 $query = "SELECT username from login where username = '$user_check'";
 $ses_sql = mysqli_query($conn, $query);
 $row = mysqli_fetch_assoc($ses_sql);
