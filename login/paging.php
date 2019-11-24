b2e00718236cc8a0f234ba3fa8b4129d2e881b5e
Bilal Ahmed <49191880+bilal-ahmed321@users.noreply.github.com>
2019-11-24 18:33:33 +0500

    Update paging.php
    paging of tables=ahmed,

--- login/paging.php
+++ login/paging.php
@@ -39,7 +39,7 @@ for ($x=$initial_num; $x<$condition_limit_num; $x++) {
 if($page<$total_pages){
     echo "<li class='page-item'><a class='page-link' href='" .$page_url. "page={$total_pages}' title='Last page is {$total_pages}.'>";
         echo "Last";
-    echo "</a></li>";
+    echo "</a></li>";  
 }
  
 echo "</ul>";
