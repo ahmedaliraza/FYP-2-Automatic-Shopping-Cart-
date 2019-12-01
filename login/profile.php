c9ee037edaf96f1c9273367807cf97ddd2131a2b
Bilal Ahmed <49191880+bilal-ahmed321@users.noreply.github.com>
2019-12-01 19:19:25 +0500

    Update profile.php

--- login/profile.php
+++ login/profile.php
@@ -1,7 +1,7 @@
 <?php
 include('session.php');
 if(!isset($_SESSION['login_user'])){
-header("location: index.php"); // Redirecting To Home Page
+header("location: index.php"); // Redirecting To Home Page    
 }
 
 ?>
