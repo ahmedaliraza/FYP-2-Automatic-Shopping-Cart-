676d82d9970de5eca5690055923acb6f410bcc75
Bilal Ahmed <49191880+bilal-ahmed321@users.noreply.github.com>
2019-12-01 18:44:38 +0500

    Update searchbill.php

--- login/searchbill.php
+++ login/searchbill.php
@@ -5,7 +5,7 @@
   <title>CARTIGO Management System </title>  
   <script src="js/jquery-1.10.2.min.js"></script>
   <link rel="stylesheet" href="css/bootstrap.min.css" />
-  <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>    
 
  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
