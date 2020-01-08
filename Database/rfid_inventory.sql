a49bdbf5798ef8e6ab407d7c12a3a8ba4eb095a9
hamza846 <55677165+hamza846@users.noreply.github.com>
2020-01-08 16:32:11 +0500

    Update rfid_inventory.sql

--- Database/rfid_inventory.sql
+++ Database/rfid_inventory.sql
@@ -7,7 +7,7 @@
 -- Server version: 10.4.6-MariaDB              
 -- PHP Version: 7.1.32         
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";        
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
