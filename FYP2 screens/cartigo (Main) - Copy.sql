c8900863cf5c72568c683770ad11cc99a0cbb16c
hamza846 <55677165+hamza846@users.noreply.github.com>
2020-01-08 17:06:04 +0500

    Update cartigo (Main) - Copy.sql

--- FYP2 screens/cartigo (Main) - Copy.sql
+++ FYP2 screens/cartigo (Main) - Copy.sql
@@ -5,7 +5,7 @@
 -- Host: 127.0.0.1
 -- Generation Time: Oct 20, 2019 at 02:07 PM
 -- Server version: 10.4.6-MariaDB
--- PHP Version: 7.1.32
+-- PHP Version: 7.1.32    
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
